1fc716a2ca27feefa4ed01e3dd4befff5cba8abe
Rameez Akther M <145257146+RameezAkther@users.noreply.github.com>
2025-09-06 15:38:16 +0530

    Update infer_v2.py

--- rtdetr_pytorch/tools/infer_v2.py
+++ rtdetr_pytorch/tools/infer_v2.py
@@ -7,10 +7,9 @@ import torchvision.transforms as T
 from torch.cuda.amp import autocast
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
-from src.core import YAMLConfig
 import sys
-
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+from src.core import YAMLConfig
 
 # ✅ Assign unique colors for each class
 def get_color_map(num_classes=80, seed=42):
